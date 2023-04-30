9e74c7f3de0e19ba0dabcaf856e19532f2a23240
Bayram Enes YILMAZ <63585653+bayramenesyilmaz@users.noreply.github.com>
2023-04-30 20:45:52 +0300

    pcat mongoose

--- app.js
+++ app.js
@@ -1,3 +1,44 @@
-const fn = function () {
-  console.log('Function working');
-};
+const express = require('express');
+const mongoose = require('mongoose');
+
+const ejs = require('ejs');
+const path = require('path');
+const Photo = require('./models/Photo');
+
+const app = express();
+
+// connect DB
+mongoose.connect('mongodb://localhost/pcat-test-db');
+
+/* TEMPLATE ENGİNE */
+app.set('view engine', 'ejs');
+
+/* MIDDLEWARE */
+app.use(express.static('public'));
+app.use(express.urlencoded({ extends: true }));
+app.use(express.json());
+
+/* ROUTES */
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({});
+  res.render('index', { photos });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add');
+});
+
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect('/');
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server ${port} nolu portta çalışıyor...`);
+});
